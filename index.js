e124dd657f1d13645e58f99b4f28592df231f647
Ryan <coap.ryanmontgomery@gmail.com>
2024-05-13 19:45:26 -0600

    conditionals lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result;
+  if (someValue <= 200) {
+    result = `This one is on me!`;
+  } else if (someValue > 200 && someValue <= 2000) {
+    result = `That will be twenty bucks.`;
+  } else if (someValue > 2000 && someValue <= 2500) {
+    result = `I will gladly take your thirty bucks.`;
+  } else {
+    result = `No can do.`
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+// Function to check the city
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to respond based on the tip
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
